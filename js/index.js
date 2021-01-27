02566c3f5ef9b7a24de838bbd45f77df294800d7
spenser6131 <brinkman.spenser@gmail.com>
2021-01-26 20:50:53 -0800

    basic styling

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 const logo = document.getElementById('logo')
-const test = document.getElementById('leaf-button')
-console.log(test)
+const leafButton = document.getElementById('leaf-button')
 
 let srcs = {
   "file://wsl%24/Ubuntu/home/spenser6131/development/flatiron/phase-4/projects/greenhouse-frontend/images/green-leaf.png": "file://wsl%24/Ubuntu/home/spenser6131/development/flatiron/phase-4/projects/greenhouse-frontend/images/orange-leaf.png",
   "file://wsl%24/Ubuntu/home/spenser6131/development/flatiron/phase-4/projects/greenhouse-frontend/images/orange-leaf.png": "file://wsl%24/Ubuntu/home/spenser6131/development/flatiron/phase-4/projects/greenhouse-frontend/images/green-leaf.png"
 }
-test.addEventListener('click', function(event) {
-  test.src = srcs[test.src]
+
+leafButton.addEventListener('click', function(event) {
+  leafButton.src = srcs[leafButton.src]
 })
