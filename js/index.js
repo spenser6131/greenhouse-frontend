b23776cd29ec307ffaa05be74a45d817055cea26
spenser6131 <brinkman.spenser@gmail.com>
2021-01-27 16:46:52 -0800

    header

--- js/index.js
+++ js/index.js
@@ -9,3 +9,25 @@ let srcs = {
 leafButton.addEventListener('click', function(event) {
   leafButton.src = srcs[leafButton.src]
 })
+
+
+var navbar = document.querySelector(".navbar")
+var ham = document.getElementById('burger')
+let burgs = {
+  "file://wsl%24/Ubuntu/home/spenser6131/development/flatiron/phase-4/projects/greenhouse-frontend/images/green-burger.png": "file://wsl%24/Ubuntu/home/spenser6131/development/flatiron/phase-4/projects/greenhouse-frontend/images/orange-burger.png",
+  "file://wsl%24/Ubuntu/home/spenser6131/development/flatiron/phase-4/projects/greenhouse-frontend/images/orange-burger.png": "file://wsl%24/Ubuntu/home/spenser6131/development/flatiron/phase-4/projects/greenhouse-frontend/images/green-burger.png"
+}
+ham.addEventListener("click", toggleHamburger)
+
+function toggleHamburger(){
+  navbar.classList.toggle("showNav")
+  ham.src = burgs[ham.src]
+}
+
+var menuLinks = document.querySelectorAll(".menuLink")
+
+menuLinks.forEach( 
+  function(menuLink) { 
+    menuLink.addEventListener("click", toggleHamburger) 
+  }
+)
