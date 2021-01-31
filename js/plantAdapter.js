b3d2c912a1b118231c2a7959bbcfa00009baa4b6
spenser6131 <brinkman.spenser@gmail.com>
2021-01-31 10:07:15 -0800

    basic plant adapter methods

--- js/plantAdapter.js
+++ js/plantAdapter.js
@@ -1,14 +1,46 @@
 class PlantAdapter{
-
   static baseURL = "http://localhost:3000/plants"
 
   static fetchAndMakePlants(){
     return fetch(PlantAdapter.baseURL)
-      .then(res => res.json())
-      .then(function(cityArray){
-        return cityArray.forEach(function(city) {
-          return new Plant(city)
+      .then(obj => obj.json())
+      .then(function(plantsArray){
+        return plantsArray.forEach(function(plant) {
+          return new Plant(plant)
         })
       })
   }
+
+  static createPlant(body){
+    return fetch(`${PlantAdapter.baseURL}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify(body)
+    })
+  }
+  
+  static editPlant({id, species, humidity_requirement, light_requirement, water_frequency, last_watering, last_fertilization, comments}){
+    return fetch(`${PlantAdapter.baseURL}${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify({
+        plant: {
+          species,
+          humidity_requirement,
+          light_requirement,
+          water_frequency,
+          last_watering,
+          last_fertilization,
+          comments
+        }
+      })
+    })
+  }
+
 }
