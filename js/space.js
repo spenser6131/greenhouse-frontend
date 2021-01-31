6817803866e14c7f539622d9c004805e946ebe85
spenser6131 <brinkman.spenser@gmail.com>
2021-01-31 10:07:26 -0800

    better space creation

--- js/space.js
+++ js/space.js
@@ -1,69 +1,89 @@
 class Space{
 
   static all = []
-  static spaceContainer = document.getElementById('space-container')
-
-  constructor({id, name, light, water}){
-    this.id = id
-    this.name = name
-    this.light = light
-    this.water = water
+  static spacesContainer = document.getElementById('spaces-container')
+  
+  constructor({id, name, humidity, light}) {
+    this.id = id;
+    this.name = name;
+    this.humidity = humidity;
+    this.light = light;
 
     this.main = document.createElement('div')
     this.main.id = `space-${this.id}`
+    this.main.classList = `space-card`
+
+    this.holder = document.createElement('div')
+    this.holder.classList = `holder card-content`
+
     this.details = document.createElement('div')
     this.details.id = `space-${this.id}-details`
-    this.cities = document.createElement('div')
-    this.cities.id = `space-${this.id}-cities`
+
+    this.plants = document.createElement('div')
+    this.plants.id = `space-${this.id}-plants`
+
     this.editButton = document.createElement('button')
     this.editButton.innerText = "Edit Space"
-    let space = document.createElement('p')
-    space.innerText = "=================="
-    this.main.append(this.details, this.cities, this.editButton, space)
 
+    this.holder.append(this.details, this.plants, this.editButton)
+    this.main.append(this.holder)
     this.form = document.createElement('form')
-
     this.editButton.addEventListener('click', this.renderEditSpaceForm)
-    this.form.addEventListener('submit', this.submitEditSpaceFrom)
+    this.form.addEventListener('submit', this.submitEditSpaceForm)
+
     Space.all.push(this)
   }
 
   renderDetails(){
     this.details.innerHTML = `
-      <p>Name: <span>${this.name}</span></p>
-      <p>light: <span>${this.light}</span></p>
-      <p>National water: <span>${this.water}</span></p>
+    <h2 class="card_title">${this.name}</h2>
+    <p class="card_text">Light: ${this.light}</p>
+    <p class="card_text">Humidity: ${this.humidity}</p>
     `
   }
-
-  allCities(){
-    return City.all.filter(city => city.spaceId == this.id)
+  
+  allPlants(){
+    return Plant.all.filter(plant => plant.spaceId == this.id)
   }
 
-  renderCities(){
-    this.cities.innerHTML = this.allCities().map(city => city.renderLI()).join("")
+  renderPlants(){
+    this.plants.innerHTML = this.allPlants().map(plant => plant.renderLI()).join("")
   }
 
   renderEditSpaceForm = () => {
     this.editButton.disabled = true
-    console.log(this);
     this.details.innerHTML = ''
     this.details.appendChild(this.form)
+    this.deleteButton = document.createElement('button')
+    this.deleteButton.innerText = "Delete Space"
+    this.deleteButton.addEventListener("click", this.deleteSpace)
     this.form.innerHTML = `
       <label>Name:</label>
       <input type="text" name="name" value="${this.name}">
       <br/>
-      <label>light:</label>
-      <input type="text" name="light" value="${this.light}">
+      <label>Humidity Level:</label>
+      <input type="text" name="humidity" value="${this.humidity}">
       <br/>
-      <label>National water:</label>
-      <input type="text" name="water" value="${this.water}">
+      <label>Light Level:</label>
+      <input type="text" name="light" value="${this.light}">
       <br/>
       <input id=edit-space type="submit" value="Submit">
     `
+    this.form.appendChild(this.deleteButton)
   }
 
-  submitEditSpaceFrom = (e) => {
+  static submitNewSpaceForm = (e) => {
+    e.preventDefault()
+    let form = document.getElementById('new-space-form')
+    let newSpaceData = {}
+    form.querySelectorAll('input').forEach(function(input){
+      if (input.name !== "submit")
+        newSpaceData[`${input.name}`] = input.value
+    })
+    SpaceAdapter.createSpace(newSpaceData)
+  }
+
+  submitEditSpaceForm = (e) => {
     e.preventDefault()
     this.form.querySelectorAll('input').forEach(function(input){
       input.name !== "submit" && (this[`${input.name}`] = input.value)
@@ -73,11 +93,16 @@ class Space{
     SpaceAdapter.editSpace(this)
   }
 
-  static renderAllCountries(){
-    Space.all.forEach((space) => {
+  deleteSpace = (e) => {
+    SpaceAdapter.deleteSpace(this)
+  }
+
+  static renderAllSpaces(){
+    Space.all.forEach(space => {
       space.renderDetails()
-      space.renderCities()
-      Space.spaceContainer.appendChild(space.main)
+      space.renderPlants()
+      Space.spacesContainer.appendChild(space.main)
     })
   }
+
 }
