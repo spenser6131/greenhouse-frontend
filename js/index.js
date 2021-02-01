ac019609a359f3a56dde5090d6882cfb78ae4469
spenser6131 <brinkman.spenser@gmail.com>
2021-01-31 16:37:30 -0800

    minor refactor

--- js/index.js
+++ js/index.js
@@ -1,5 +1,3 @@
 PlantAdapter.fetchAndMakePlants()
 .then(SpaceAdapter.fetchAndMakeSpaces)
 .then(Space.renderAllSpaces)
-
-document.getElementById('new-space-form').addEventListener('submit', Space.submitNewSpaceForm)
